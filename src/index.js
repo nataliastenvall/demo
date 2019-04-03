f23bd2280cc6d2b2e61e73dc5565c5208a7d16cf
nataliastenvall <47786456+nataliastenvall@users.noreply.github.com>
2019-04-03 10:05:16 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Math(){
+  let x=5;
+  let y=10;
+
+  return(x+y);
+}
+
+let person = (
+  <div className="myclass">
+  <h1>Hello world!</h1>
+  <p>Enjoy my first React app.
+  It is another line of the content</p>
+  </div>
+);
+
+function Person(props){
+  return(
+    <div className="girls">
+    <h1>{props.name}</h1>
+    <p>Hobby: {props.hobby}</p>
+    <p>Height: {props.height}</p>
+    </div>);
+}
+
+var data = (
+  <div>
+<Person name="Benita" hobby="music" height="165"/>
+<Person name="Ksenia" hobby="software" height="168"/>
+<Person name="Valentina" hobby="cooking" height="170"/>
+  </div>
+);
+
+ReactDOM.render(<Math />, document.getElementById('math'));
+ReactDOM.render(person, document.getElementById('root'));
+ReactDOM.render(data, document.querySelector('#data'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
